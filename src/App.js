59ca8ad7bc573bbcffb4c69ceebeac150a297415
Milan Lakhani <milanlakhaniaiml@gmail.com>
2022-03-14 21:12:15 -0400

    sidebar and content seperated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar />
+    <div className="root-body">
+      <div className="relative min-h-screen flex">
+        {/* sidebar navigation goes here ---------- */}
+        <div className="relative nav w-60 bg-red-700 text-gray-100">
+          navigation sidebar
+        </div>
+        {/* mobile screen here ----------- */}
+        <div className="conten flex-1">
+        content goes here ---------------
+        </div>
+      </div>
     </div>
   );
 }
